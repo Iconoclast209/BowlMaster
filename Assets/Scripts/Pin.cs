e03fe2253f1c39762d1b3eeae784ac31a8ee5cac
awsmith <mrpresidentsmith@gmail.com>
2018-02-02 12:20:28 -0800

    End of Lecture 201.  Added a method to check if each pin is standing or not.

---
+++ Assets/Scripts/Pin.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour {
+
+    [Tooltip("The amount in degrees that the pin can be off of the X and Z axes and still be considered standing upright")]
+    public float standingThreshold = 5f;
+
+    private void Start()
+    {
+        IsStanding();
+    }
+
+    private void Update()
+    {
+
+    }
+
+    public bool IsStanding()
+    {
+        Vector3 rotationInEuler = transform.rotation.eulerAngles;
+
+        float xTilt = Mathf.Abs(rotationInEuler.x);
+        float zTilt = Mathf.Abs(rotationInEuler.z);
+        print(this.name +" "+ xTilt + ", " + zTilt);
+        
+        if (xTilt < standingThreshold || xTilt > (360 - standingThreshold))
+        {
+            if (zTilt < standingThreshold || zTilt > (360 - standingThreshold))
+            {
+                print("true");
+                return true;
+
+            }
+        }
+        print("false");
+        return false;
+        
+    }
+}
