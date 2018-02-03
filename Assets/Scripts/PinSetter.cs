c2afb312968419b1d023db9bf93c7c504dd1a0df
awsmith <mrpresidentsmith@gmail.com>
2018-02-02 17:05:40 -0800

    End of Lecture 202.  Added counting of standing pins and UI text representation thereof.

---
+++ Assets/Scripts/PinSetter.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PinSetter : MonoBehaviour {
+
+    public Text pinStandingCountText; 
+    private Pin[] pinArray;
+    
+
+    private void Start()
+    {
+        
+        CountStanding();
+    }
+
+
+    public int CountStanding()
+    {
+        int pinsStanding = 0;
+        pinArray = FindObjectsOfType<Pin>();
+
+        foreach (Pin p in pinArray)
+        {
+            if (p.IsStanding())
+            {
+                pinsStanding++;
+                print(p.name + " is standing.");
+            }
+        }
+        pinStandingCountText.text = pinsStanding.ToString();
+        return pinsStanding;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.GetComponent<Ball>())
+        {
+            CountStanding();
+            Invoke("CountStanding", 2f);
+        }
+    }
+
+}
