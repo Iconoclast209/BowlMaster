7d2fcd3b86d22d831a047effd1735f6d263f9a6f
asmith <mrpresidentsmith@gmail.com>
2018-03-04 14:33:11 -0800

    End of Lecture 218.  First Test Pass Run completed.

---
+++ Assets/Editor/ActionMasterTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+[TestFixture]
+public class ActionMasterTest
+{
+    private ActionMaster.Action endTurn = ActionMaster.Action.EndTurn;
+    private ActionMaster.Action reset = ActionMaster.Action.Reset;
+
+    [Test]
+    public void T00PassingTest()
+    {
+        Assert.AreEqual(1, 1);
+    }
+
+    [Test]
+    public void T01OneStrikeReturnsEndTurn()
+    {
+        ActionMaster actionMaster = new ActionMaster ();
+        Assert.AreEqual(endTurn, actionMaster.Bowl(10));
+    }
+
+    [Test]
+    public void T02FirstStrikeReturnReset()
+    {
+        ActionMaster actionMaster = new ActionMaster();
+        Assert.AreEqual(reset, actionMaster.Bowl(10));
+    }
+}
